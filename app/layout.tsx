24ced75a8e881f24f7ae455ab8cc08f6a83b0532
Ku Mohd Syafiq <kumsyafiq.ka@trinitywizards.com>
2022-11-09 22:32:27 +0800

    init

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable @next/next/no-head-element */
+
+import Link from "next/link";
+import "./global.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <head></head>
+      <body>
+        <main>
+          {/* Might be a good place for global navbar or footer */}
+          <nav>
+            <Link href="/">Home</Link>
+            <Link href="/notes">Notes</Link>
+          </nav>
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type Props = {};
+
+export default function page({}: Props) {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Some content</p>
+    </div>
+  );
+}
